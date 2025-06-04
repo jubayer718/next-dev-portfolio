eba4519c83a9ec5f63f27b8f1e845bb3a23f7d2f
jubayer718 <jubayerahmed2462@gmail.com>
2025-06-04 09:18:43 +0600

    tailwind and globalcss setup done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import {JetBrains_Mono} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
-});
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable:"--font-jetbrainsMono",
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+})
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={jetbrainsMono.variable}
       >
         {children}
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,34 @@ module.exports = {
       sm: '640px',
       md: '768px',
       lg:  '960px',
-      xl:'1200px'
+      xl:'1200px',
     },
-    extend: {},
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
+    },
+    extend: {
+      colors: {
+        primary: '#1c1c22',
+        accent: {
+          DEFAULT: '#00ff99',
+          hover:'#00e1',
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to:{height:"var(--radix-accordion-content-height)"},
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height" },
+          to:{height:"0"}
+        }
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up":"accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss")],
 };
