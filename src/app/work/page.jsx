7a3618f30ad22552222f3e69617345010c5bf0c1
jubayer718 <jubayerahmed2462@gmail.com>
2025-06-06 23:51:53 +0600

    Project showcasing

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,11 +1,178 @@
-import React from 'react';
+"use client";
+import { motion } from 'framer-motion';
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowBarUp, BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const projects = [
+  {
+    num: "01",
+    category: "Fullstack",
+    title:"Project 01",
+    description: "News Fusion is a modern, feature-rich news platform where users can explore the latest articles, trending news, and premium content. The platform supports multiple publishers and provides an engaging user experience with interactive elements.",
+    stack: [{ name: "React.js" }, { name: "Express.js" }, { name: "Tailwind.css" }, { name: "MongoDB" }],
+    image: '/assets/work/project1.png',
+    live: "https://newfusion-f31a5.web.app/",
+    github: "https://github.com/jubayer718/news-fusion-client",
+    
+  },
+  {
+    num: "02",
+    category: "Fullstack",
+    title:"Project 02",
+    description: "BrainBuddies is a fully functional group-study web application designed for collaborative assignment management. Users can create, submit, and review assignments in an organized, secure, and intuitive environment. Built with the MERN stack (MongoDB, Express.js, React, Node.js), the platform is ideal for exam-related activities and academic collaboration.",
+    stack: [{ name: "React.js" }, { name: "Express.js" }, { name: "Tailwind.css" }, { name: "MongoDB" }],
+    image: '/assets/work/project2.png',
+    live: "https://brainbuddies-4647a.web.app/",
+    github: "https://github.com/jubayer718/brainbuddie-client",
+    
+  },
+  {
+    num: "03",
+    title:"Project 3",
+    category: "Fullstack",
+    description: "This project is a crowdfunding platform designed to help individuals, startups, and communities raise funds for their ideas. Users can create campaigns, contribute to causes they believe in, and track progress—all in a seamless and user-friendly interface.",
+    stack: [{ name: "React.js" }, { name: "Express.js" }, { name: "Tailwind.css" }, { name: "MongoDB" }],
+    image: '/assets/work/crowd_funding.png',
+    live: "https://crowd-funding-85c71.web.app/",
+    github: "https://github.com/jubayer718/crowdfund-client",
+    
+  },
+]
 
 const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+
+  const handleSlideChange = (swiper) => {
+    // get current slide index
+    const currentIndex = swiper.activeIndex;
+    // update project state based on current slide index
+    setProject(projects[currentIndex]);
+
+  }
   return (
-    <div>
-      This is working page.
-    </div>
-  );
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className='min-h-[80vh] flex-col justify-center py-12 xl:px-0'
+    >
+      <div className='container mx-auto'>
+        <div className='flex flex-col xl:flex-row xl:gap-[30px]'>
+          <div className='w-full xl:w-[50%] xl-h-[460px] flex flex-col xl:justify-between order-2 xl:order-none'>
+            <div className='flex flex-col gap-[30px] h-[50%]'>
+          {/* outline num */}
+            <div className='text-8xl leading-none font-extrabold text-transparent text-outline'>
+            {project.num}
+              </div>
+              
+            {/* project category */}
+            <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-[#00ff99] transition-all duration-500 capitalize'>
+            { project.category} project
+              </h2>
+              {/* project description */}
+              <p className='text-white/60'>{project.description}</p>
+              {/* stack */}
+              <ul className='flex gap-4'>
+                {
+                  project.stack.map((item, index) => {
+                    return (
+                      <li key={index} className='text-xl text-[#00ff99]'>
+                        {item.name}
+                        {/* remove the last comma */}
+                        {index!== project.stack.length-1 && ","}
+                      </li>
+                    )
+                  })
+                }
+
+              </ul>
+                
+              {/* border */}
+              <div className='border border-white/20'> </div>
+
+                {/* button */}
+              <div className='flex items-center gap-4'>
+                {/* Live project button */}
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className='text-white text-3xl group-hover:text-[#00ff99]'/>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+              </Link>
+                {/* Github project button */}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className='text-white text-3xl group-hover:text-[#00ff99]'/>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+              </Link>
+              </div>
+             
+
+             
+              </div>
+          </div>
+          
+          <div className='w-full xl:w-[50%]'>
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className='xl:h-[520px] mb-12'
+            onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (<SwiperSlide key={index} className='w-full'>
+                  <div className='h-[460px] relative group flex justify-center items-center bg-pink-50/20'>
+                    {/* overlay */}
+                    <div></div>
+                    {/* Image */}
+                    <div className='relative w-full h-full '>
+                      <Image
+                        src={project.image}
+                        fill 
+                        className=' object-cover'
+                        alt={project.title}
+                      ></Image>
+                    </div>
+
+                  </div>
+                  
+                </SwiperSlide>)
+              })}
+           </Swiper>
+
+          </div>
+
+        </div>
+
+      </div>
+  </motion.section>
+  )
 };
 
 export default Work;
